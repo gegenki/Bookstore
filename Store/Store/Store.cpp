a8a255b34c0c3ba450dbfa244d2f69e63731940a
Jordan <gegenki@gmail.com>
2014-03-10 21:55:08 +0000

    Basic project items

---
+++ Store/Store/Store.cpp
@@ -0,0 +1,12 @@
+// Store.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	std::cout << "hello\n";
+	return 0;
+}
+
